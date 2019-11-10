3f135393f1c5bb77aa8854fce92acd98ded93fd6
jacob-blomq <35968186+jacob-blomq@users.noreply.github.com>
2019-11-10 15:01:13 -0500

    Add files via upload

---
+++ RandomNumberAdder.java
@@ -0,0 +1,29 @@
+package Midterm;
+
+import java.util.ArrayList;
+import java.util.Random;
+public class RandomNumberAdder {
+	
+	ArrayList<Integer> numList;
+	Random rand = new Random();
+	int sum;
+	
+	public RandomNumberAdder() {
+		numList = new ArrayList<Integer>();
+		sum =0;
+	}
+	
+	public void addRandomNumbers(int howMany, int howRandom) {
+		for (int i = 0; i < howMany; i++) {
+			int rand_int = rand.nextInt(howRandom);
+			numList.add(rand_int);
+			sum = sum + rand_int;
+		}
+	}
+	
+	public static void main(String[] args) {
+		RandomNumberAdder instance = new RandomNumberAdder();
+		instance.addRandomNumbers(10, 100);
+		System.out.println("The numbers are " + instance.numList +  " and the sum is " + instance.sum);
+	}
+}
